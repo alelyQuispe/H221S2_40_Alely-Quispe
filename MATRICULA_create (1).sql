9722669c1d9cdaa8a2ef1578ef9423a06f9f7e26
Alely Quispe <112032872+alelyQuispe@users.noreply.github.com>
2022-12-13 18:19:55 -0500

    scrip SQL diseño fisico

---
+++ MATRICULA_create (1).sql
@@ -0,0 +1,16 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2022-12-13 23:05:35.189
+
+-- tables
+-- Table: MATRICULA
+CREATE TABLE MATRICULA (
+    NOMMATR varchar(100)  NOT NULL,
+    GMAMATR varchar(100)  NOT NULL,
+    CELMATR char(9)  NOT NULL,
+    ASUMATR varchar(100)  NOT NULL,
+    DESMATR varchar(100)  NOT NULL,
+    CONSTRAINT MATRICULA_pk PRIMARY KEY (NOMMATR)
+);
+
+-- End of file.
+
